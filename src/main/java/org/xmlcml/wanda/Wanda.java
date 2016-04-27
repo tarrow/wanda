63d6200914a04b4ab2100a246675d29ab16c2f52
Thomas Arrow <thomasarrow@gmail.com>
2016-04-27 15:06:37 +0100

    Initial Commit. Name Project

---
+++ src/main/java/org/xmlcml/wanda/Wanda.java
@@ -0,0 +1,12 @@
+package org.xmlcml.wanda;
+
+public class Wanda {
+
+	public void main(String[] args) {
+		// parse arguments from command line - what to summarise (all plugins?)
+		// Open CProject as specified by command line
+		// Build SnippetsTree and ProjectSnippets Trees in memory
+		// Write HTML tables
+	}
+	
+}
